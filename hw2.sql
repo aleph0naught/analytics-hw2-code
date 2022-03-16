61e1ebe9770a76642bd5aa06de581145d7f33a8c
Aleksandr Orlov <56632788+aleph0naught@users.noreply.github.com>
2022-03-16 16:31:47 +0300

    Add files via upload

--- hw2.sql
+++ hw2.sql
@@ -92,6 +92,25 @@ GRANT ALL PRIVILEGES ON dmitrkozlovsk.cdm_orders TO PUBLIC;
 drop table if exists dmitrkozlovsk.agg_ceo_new;
 create table dmitrkozlovsk.agg_ceo_new as
 select
+    'hour' as time_type
+    ,date_trunc('hour', utc_created_dttm) as time_value
+    ,city_name
+    ,is_fast_food_flg
+    ,asap_flg
+    ,place_name
+    ,delivery_type
+    ,sum(case when status = 4 then 1 else 0 end) as finish_count
+    ,sum(case when status = 4 and rating is not null then 1 else 0 end) as finish_rating_count
+    ,sum(rating) as rating_sum
+    ,sum(case when not code ~ '^(duplicate|client)' then 1 else 0 end) as cancelled_count
+    ,sum(case when status = 4 then cost_for_customer else 0 end) as earned_sum
+    ,count(distinct user_id) as user_count
+from
+     dmitrkozlovsk.cdm_orders
+group by
+    1, 2, 3, 4, 5, 6, 7
+union all
+select
     'day' as time_type
     ,date_trunc('day', utc_created_dttm) as time_value
     ,city_name
@@ -153,6 +172,19 @@ GRANT ALL PRIVILEGES ON dmitrkozlovsk.agg_ceo_new TO PUBLIC;
 drop table if exists dmitrkozlovsk.agg_dev;
 create table dmitrkozlovsk.agg_dev as
 select
+    'hour' as time_type
+    ,date_trunc('hour', utc_created_dttm) as time_value
+    ,application_platform
+    ,split_part(code, '.', 1) as cancel_type
+    ,code as cancel_subtype
+    ,sum(case when status = 4 then 1 else 0 end) as finish_count
+    ,sum(case when code is null then 0 else 1 end) as canceled_count
+from
+     dmitrkozlovsk.cdm_orders
+group by
+    1, 2, 3, 4, 5
+union all
+select
     'day' as time_type
     ,date_trunc('day', utc_created_dttm) as time_value
     ,application_platform
@@ -195,6 +227,86 @@ GRANT ALL PRIVILEGES ON dmitrkozlovsk.agg_dev TO PUBLIC;
 
 drop table if exists dmitrkozlovsk.agg_delivery;
 create table dmitrkozlovsk.agg_delivery as
+select
+    'hour' as time_type
+    ,date_trunc('hour', utc_created_dttm) as time_value
+    ,city_name
+    ,is_fast_food_flg
+    ,asap_flg
+    ,place_name
+    ,case when code ~ '^(courier)' then 1 else 0 end as courier_cancelled
+    ,case when split_part(code, '.', 1) = 'courier' then split_part(code, '.', 2) end as courier_cancel_reason
+    ,delivery_type
+    ,sum(case when status = 4 then 1 else 0 end) as finish_count
+    ,sum(case when code is not null and code ~ '^(courier)' then 1 else 0 end) as courier_canceled_count
+    ,sum(case when code is not null and not code ~ '^(duplicate|courier)' then 1 else 0 end) as other_canceled_count
+    ,sum(case when code is not null and code ~ '^(courier)' then cost_for_customer else 0 end) as courier_sum
+    ,sum(case when status = 4 then cost_for_customer else 0 end) as earned_sum
+    ,sum(case when code is not null and not code ~ '^(duplicate|courier)' then cost_for_customer else 0 end) as other_sum
+    ,count(distinct courier_id) as user_count
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then extract(hours from utc_delivered_dttm - utc_taken_dttm)*60 + extract(minutes from utc_delivered_dttm - utc_taken_dttm) else 0 end) as delivered_to_taken_time
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then extract(hours from utc_taken_dttm - utc_courier_arrived_to_place_dttm)*60 + extract(minutes from utc_taken_dttm - utc_courier_arrived_to_place_dttm)else 0 end) as taken_to_courier_arrived_time
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then extract(hours from utc_courier_arrived_to_place_dttm - utc_adopted_by_courier_dttm) * 60 + extract(minutes from utc_courier_arrived_to_place_dttm - utc_adopted_by_courier_dttm) else 0 end) as courier_arrived_to_adopted_time
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then extract(hours from utc_adopted_by_courier_dttm - utc_courier_assigned_dttm)*60 + extract(minutes from utc_adopted_by_courier_dttm - utc_courier_assigned_dttm) else 0 end) as adopted_to_courier_assigned_time
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then extract(hours from utc_courier_assigned_dttm - utc_created_dttm)*60+extract(minutes from utc_courier_assigned_dttm - utc_created_dttm) else 0 end) as courier_assigned_to_created_time
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then time_to_delivery - pre_delivery_time else 0 end) as pre_time_diff
+    ,sum(time_to_delivery) as time_to_delivery_sum
+    ,sum(extract(hours from utc_delivered_dttm - utc_created_dttm)*60+extract(minutes from utc_delivered_dttm - utc_created_dttm)) as click_to_eat
+    ,sum(case when delivery_type = 'native'
+    and cancel_reason_id is null
+    and not (utc_courier_assigned_dttm is null
+            or utc_adopted_by_courier_dttm is null
+            or utc_place_confirmed_dttm is null
+            or utc_courier_arrived_to_place_dttm is null
+            or utc_taken_dttm is null
+            or utc_delivered_dttm is null) then 1 else 0 end) as time_diff_count
+from
+     dmitrkozlovsk.cdm_orders
+group by
+    1, 2, 3, 4, 5, 6, 7, 8, 9
+union all
 select
     'day' as time_type
     ,date_trunc('day', utc_created_dttm) as time_value
@@ -438,6 +550,27 @@ GRANT ALL PRIVILEGES ON dmitrkozlovsk.agg_delivery TO PUBLIC;
 
 drop table if exists dmitrkozlovsk.agg_place;
 create table dmitrkozlovsk.agg_place as
+select
+    'hour' as time_type
+    ,date_trunc('hour', utc_created_dttm) as time_value
+    ,city_name
+    ,is_fast_food_flg
+    ,asap_flg
+    ,place_name
+    ,cancel_reason_type
+    ,sum(case when code ~ '^(place)' then 1 else 0 end) as cancelled_count
+    ,sum(case when status = 4 then 1 else 0 end) as finish_count
+    ,count(place_id) as place_id_num
+    ,case when split_part(code, '.', 1) = 'place' then split_part(code, '.', 2) end as cancel_reason
+    ,sum(case when not code ~ '^(place|duplicate)' then 1 else 0 end) as other_count
+    ,sum(case when code ~ '^(place)' then cost_for_customer end) as place_revenue
+    ,sum(case when not code ~ '^(place|duplicate)' then cost_for_customer end) as other_revenue
+    ,sum(case when status = 4 then cost_for_customer end) as finished_revenue
+from
+     dmitrkozlovsk.cdm_orders
+group by
+    1, 2, 3, 4, 5, 6, 7, 11
+union all
 select
     'day' as time_type
     ,date_trunc('day', utc_created_dttm) as time_value
